fd40984b5d198af31dd49c919287ac613082855d
AtharvaRanaware <69978554+AtharvaRanaware@users.noreply.github.com>
2020-09-13 15:57:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,110 @@
+
+var wall,thickness;
+
+var bullet,speed,weight;
+
+
+
+
+
+
+function setup() {
+  createCanvas(1600,400);
+
+   
+  speed=random(223,320);
+  
+  weight=random(30,52)
+
+  thickness=random(22,83)
+
+  bullet=createSprite(30,200,50,10)
+  bullet.velocityX=speed;
+  bullet .shapeColor=color('white')
+
+  wall=createSprite(1400,200,thickness,height/2)
+   wall .shapeColor=color('grey')
+ 
+
+
+
+  
+}
+
+function draw() {
+  background('blue');
+ 
+if (hasCollided(bullet,wall)) 
+{
+bullet.velocityX=0;
+
+var damage=0.5 * speeed * weight* speed/(thickness * thickness *thickness)
+
+if (damage>10)
+{
+  wall .shapeColor=color('red')
+
+
+}
+
+if (damage<10)
+{
+  wall .shapeColor=color('green')
+
+
+}
+
+if (damage=10)
+{
+  wall .shapeColor=color('yellow')
+
+
+}
+
+
+
+}
+
+
+ 
+
+  drawSprites();
+}
+
+
+function hasCollided(lbullet,lwall)
+{
+bulletRightEdge=lbullet.x+lbullet.width;
+
+wallLeftEdge=lwall.x;
+  
+  if (bulletRightEdge>=wallLeftEdge) 
+  {
+
+   return true
+
+
+  }
+
+  return false
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
